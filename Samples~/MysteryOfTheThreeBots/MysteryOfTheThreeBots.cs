b347b73a5795597a0ea73907ac3d511b8c6c53f1
Antonis Makropoulos <benuix@gmail.com>
2024-03-01 14:36:50 +0200

    full game

--- Samples~/MysteryOfTheThreeBots/MysteryOfTheThreeBots.cs
+++ Samples~/MysteryOfTheThreeBots/MysteryOfTheThreeBots.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Diagnostics;
 using Debug = UnityEngine.Debug;
 using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class MysteryOfTheThreeBots : MonoBehaviour
 {
@@ -20,6 +21,24 @@ public class MysteryOfTheThreeBots : MonoBehaviour
     public RawImage ChefImage;
     public Embedding embedding;
 
+    public Button NotesButton;
+    public Button MapButton;
+    public Button SolveButton;
+    public Button HelpButton;
+
+    public RawImage NotebookImage;
+    public GameObject NotesPanel;
+    public GameObject SolvePanel;
+    public GameObject HelpPanel;
+
+    public RawImage MapImage;
+    public Button SubmitButton;
+    public RawImage SuccessImage;
+    public Text FailText;
+    public Dropdown Answer1;
+    public Dropdown Answer2;
+    public Dropdown Answer3;
+
     Dictionary<string, Bot> bots;
     Dictionary<string, RawImage> botImages;
     string currentBotName;
@@ -28,8 +47,21 @@ public class MysteryOfTheThreeBots : MonoBehaviour
 
     void Start()
     {
+        AddListeners();
         StartCoroutine(InitDialogue());
+    }
+
+    void AddListeners()
+    {
         CharacterSelect.onValueChanged.AddListener(DropdownChange);
+        NotesButton.onClick.AddListener(ShowNotes);
+        MapButton.onClick.AddListener(ShowMap);
+        SolveButton.onClick.AddListener(ShowSolve);
+        HelpButton.onClick.AddListener(ShowHelp);
+        SubmitButton.onClick.AddListener(SubmitAnswer);
+        Answer1.onValueChanged.AddListener(HideFail);
+        Answer2.onValueChanged.AddListener(HideFail);
+        Answer3.onValueChanged.AddListener(HideFail);
     }
 
     IEnumerator<string> InitDialogue()
@@ -101,6 +133,68 @@ public class MysteryOfTheThreeBots : MonoBehaviour
         Debug.Log($"{currentBotName}: {currentBot.NumPhrases()} phrases available");
     }
 
+    void ShowNotes()
+    {
+        NotesPanel.gameObject.SetActive(true);
+        HelpPanel.gameObject.SetActive(false);
+        SolvePanel.gameObject.SetActive(false);
+        NotebookImage.gameObject.SetActive(true);
+    }
+
+    void ShowMap()
+    {
+        MapImage.gameObject.SetActive(true);
+    }
+
+    void HideFail(int selection)
+    {
+        FailText.gameObject.SetActive(false);
+    }
+
+    void ShowSolve()
+    {
+        HideFail(0);
+        NotesPanel.gameObject.SetActive(false);
+        HelpPanel.gameObject.SetActive(false);
+        SolvePanel.gameObject.SetActive(true);
+        NotebookImage.gameObject.SetActive(true);
+    }
+
+    void ShowHelp()
+    {
+        NotesPanel.gameObject.SetActive(false);
+        HelpPanel.gameObject.SetActive(true);
+        SolvePanel.gameObject.SetActive(false);
+        NotebookImage.gameObject.SetActive(true);
+    }
+
+    void SubmitAnswer()
+    {
+        if(Answer1.options[Answer1.value].text == "Professor Pluot" && Answer2.options[Answer2.value].text == "Living Room" && Answer3.options[Answer3.value].text == "A Hollow Bible")
+        {
+            NotebookImage.gameObject.SetActive(false);
+            SuccessImage.gameObject.SetActive(true);
+        }
+        else
+        {
+            FailText.gameObject.SetActive(true);
+        }
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            foreach (RawImage image in new RawImage[]{NotebookImage, MapImage, SuccessImage})
+            {
+                if (image.IsActive() && !RectTransformUtility.RectangleContainsScreenPoint(image.rectTransform, Input.mousePosition))
+                {
+                    image.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
 }
 
 
